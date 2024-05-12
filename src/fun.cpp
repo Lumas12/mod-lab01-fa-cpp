c7ee8d5748154ec23ace442e8c91e139bed839cf
Максим <120325159+Lumas12@users.noreply.github.com>
2024-05-12 14:47:31 +0300

    Львов Максим 382007-3 fun.cpp
    попытка 4

--- src/fun.cpp
+++ src/fun.cpp
@@ -12,21 +12,54 @@ unsigned int countWords(const char *const str) {
 }
 
 unsigned int faStr1(const char *const str) {
-    return std::count_if(str, str + std::strlen(str), [](char c) {
-        return std::isdigit(c);
-    }) == 0 ? countWords(str) : 0;
+    unsigned int count = 0;
+    bool inWord = false;
+
+    for (const char *ptr = str; *ptr; ++ptr) {
+        if (!inWord && !std::isdigit(*ptr)) {
+            inWord = true;
+            ++count;
+        } else if (inWord && std::isspace(*ptr)) {
+            inWord = false;
+        }
+    }
+
+    return count;
 }
 
 unsigned int faStr2(const char *const str) {
-    return std::count_if(str, str + std::strlen(str), [](char c) {
-        return std::isupper(c);
-    });
+    unsigned int count = 0;
+    bool newWord = true;
+
+    for (const char *ptr = str; *ptr; ++ptr) {
+        if (newWord && std::isupper(*ptr)) {
+            ++count;
+            newWord = false;
+        } else if (std::isspace(*ptr)) {
+            newWord = true;
+        }
+    }
+
+    return count;
 }
 
 unsigned int faStr3(const char *const str) {
-    unsigned int wordCount = countWords(str);
-    unsigned int charCount = std::count_if(str, str + std::strlen(str), [](char c) {
-        return !std::isspace(c);
-    });
-    return std::round(static_cast<double>(charCount) / wordCount);
+    unsigned int sumLength = 0;
+    unsigned int wordCount = 0;
+    bool inWord = false;
+
+    for (const char *ptr = str; *ptr; ++ptr) {
+        if (!inWord && !std::isspace(*ptr)) {
+            inWord = true;
+            ++wordCount;
+        }
+        if (inWord && !std::isspace(*ptr)) {
+            ++sumLength;
+        }
+        if (inWord && (std::isspace(*ptr) || !*(ptr + 1))) {
+            inWord = false;
+        }
+    }
+
+    return sumLength / wordCount;
 }
