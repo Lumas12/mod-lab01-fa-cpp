bda693bd10bea9cd38607cb180e2af608c8c5cce
Максим <120325159+Lumas12@users.noreply.github.com>
2024-05-12 15:33:44 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -27,39 +27,3 @@ unsigned int faStr1(const char *const str) {
     return count;
 }
 
-unsigned int faStr2(const char *const str) {
-    unsigned int count = 0;
-    bool newWord = true;
-
-    for (const char *ptr = str; *ptr; ++ptr) {
-        if (newWord && std::isupper(*ptr)) {
-            ++count;
-            newWord = false;
-        } else if (std::isspace(*ptr)) {
-            newWord = true;
-        }
-    }
-
-    return count;
-}
-
-unsigned int faStr3(const char *const str) {
-    unsigned int sumLength = 0;
-    unsigned int wordCount = 0;
-    bool inWord = false;
-
-    for (const char *ptr = str; *ptr; ++ptr) {
-        if (!inWord && !std::isspace(*ptr)) {
-            inWord = true;
-            ++wordCount;
-        }
-        if (inWord && !std::isspace(*ptr)) {
-            ++sumLength;
-        }
-        if (inWord && (std::isspace(*ptr) || !*(ptr + 1))) {
-            inWord = false;
-        }
-    }
-
-    return sumLength / wordCount;
-}
