cf50876f560ae9053b3e63574533d2331b061665
Максим <120325159+Lumas12@users.noreply.github.com>
2024-05-12 14:14:17 +0300

    Update fun.cpp
    попытка первая

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,31 @@
-// Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <algorithm>
+#include <cmath>
+#include <cctype>
+#include <iterator>
 
-unsigned int faStr1(const char *str) {
-    return 0;
+unsigned int countWords(const char *const str) {
+    return std::count_if(str, str + std::strlen(str), [](char c) {
+        return !std::isspace(c);
+    });
 }
 
-unsigned int faStr2(const char *str) {
-    return 0;
+unsigned int faStr1(const char *const str) {
+    return std::count_if(str, str + std::strlen(str), [](char c) {
+        return std::isdigit(c);
+    }) == 0 ? countWords(str) : 0;
 }
 
-unsigned int faStr3(const char *str) {
-    return 0;
+unsigned int faStr2(const char *const str) {
+    return std::count_if(str, str + std::strlen(str), [](char c) {
+        return std::isupper(c);
+    });
+}
+
+unsigned int faStr3(const char *const str) {
+    unsigned int wordCount = countWords(str);
+    unsigned int charCount = std::count_if(str, str + std::strlen(str), [](char c) {
+        return !std::isspace(c);
+    });
+    return std::round(static_cast<double>(charCount) / wordCount);
 }
